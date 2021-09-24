7a0d919fb9b1ec3ead48cbb2329feea606ffed72
Gaurav Singh <83450530+garv-010@users.noreply.github.com>
2021-09-24 20:39:17 +0530

    Add files via upload

---
+++ Linear_Search/RichestCustomerWealth.java
@@ -0,0 +1,28 @@
+package kunalwstDSA.searching_Algo;
+
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+
+    }
+    public int maximumWealth(int[][] accounts) {
+        // person  = rol
+         // account = col
+        int ans = Integer.MIN_VALUE;
+
+        for(int person = 0 ; person<accounts.length ; person++) {
+            // when you start a new col , take a new sum for that row .
+            int sum = 0;
+            for(int account = 0 ; account < accounts[person].length ; account++) {
+                sum += accounts[person][account];
+            }
+
+            // now we have sum of accounts of person
+            // check with overall ans
+            if(sum > ans) {
+                ans = sum;
+            }
+        }
+        return ans;
+
+    }
+}
