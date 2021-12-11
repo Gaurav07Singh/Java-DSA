6a731249fdc0bf7a05d4a318743b8e90c373fef5
Gaurav Singh <83450530+Gaurav07Singh@users.noreply.github.com>
2021-12-11 22:13:45 +0530

    Update SelectionSort.java

--- Sorting_Algorithms/SelectionSort.java
+++ Sorting_Algorithms/SelectionSort.java
@@ -1,26 +1,37 @@
 package kunalwstDSA.sorting_Algos;
 
-public class SelectionSort {
+import java.util.Arrays;
+
+public class Prac1 {
     public static void main(String[] args) {
 
-        int a[] = {3 ,1 , -2 ,7, 5 ,0};
-        int n = a.length;
+        int[] arr = {0,1,5,6,2,4,3};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
 
-        for(int i = 0 ; i<n-1 ; i++) {
-            int minInd = i;
-            for(int j = i ; j< n ; j++) {
-                if(a[j] < a[minInd]) {
-                    minInd = j;
-                }
-            }
-            int temp = a[i];
-            a[i] = a[minInd];
-            a[minInd] = temp;
-        }
-        for(int e : a) {
-            System.out.println(e);
-        }
 
     }
+    static void selection(int[] arr) {
+        for(int i = 0 ; i<arr.length ;i++) {
+            // find the max item in array and swap with correct index.
+            int last = arr.length-i-1;
+            int maxIndex = getMaxIndex(arr,0,last);
+            swap(arr,maxIndex,last);
 
+        }
+    }
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second]=temp;
+    }
+    private static int getMaxIndex(int[] arr , int start , int end) {
+        int max = start;
+        for(int i = start ; i<=end ; i++) {
+            if(arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
 }
