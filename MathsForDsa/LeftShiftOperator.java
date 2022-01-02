8fae8eb992b7c39aa70484e6c85cdd8a39311655
Gaurav Singh <83450530+Gaurav07Singh@users.noreply.github.com>
2022-01-02 18:31:10 +0530

    Add files via upload

---
+++ MathsForDsa/LeftShiftOperator.java
@@ -0,0 +1,18 @@
+package kunalwstDSA.maths.operators;
+
+public class LeftShiftOperator {
+    public static void main(String[] args) {
+
+//        int a = 10;
+//        int b =a <<5;
+//        System.out.println(b);
+//
+        // a << 1 = 2 * a;
+        // a << b = a * 2^b;
+
+        int a=15;
+
+        int c = a >> 1;
+        System.out.println(c);
+    }
+}
