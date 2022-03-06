8080469f61ac2cc4187f8d6e7aaf47ba14f280b4
Gaurav Singh <83450530+Gaurav07Singh@users.noreply.github.com>
2022-03-06 23:20:31 +0530

    Update FiboFormula.java

--- Assignments/Recursion/FiboFormula.java
+++ Assignments/Recursion/FiboFormula.java
@@ -7,6 +7,6 @@ class FiboFormula {
 
 	}
 	static int fibo(int n) {
-		return (int)((Math.pow(((1 + Math.sqrt(5)) / 2 ) ,n) - Math.pow(((1 - Math.sqrt(5)) /2), n)) / Math.sqrt(5));
+		 return (int)(Math.pow(((1 + Math.sqrt(5)) / 2 ) ,n)/ Math.sqrt(5));
 	}
 }
